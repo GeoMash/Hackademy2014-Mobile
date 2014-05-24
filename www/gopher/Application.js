07e7a39906c09c9a000d5834ad332cacb23a4548
Timothy Chandler <tim@praxisbt.com>
2014-05-24 18:08:21 +0800

    Adding panel.

--- www/gopher/Application.js
+++ www/gopher/Application.js
@@ -16,7 +16,29 @@ $JSKK.Class.create
 		},
 		onReady: function()
 		{
-			$("[data-role='header'], [data-role='footer']").toolbar({theme:'b'});
+			$('[data-role="header"], [data-role="footer"]').toolbar({theme:'b'});
+			$('[data-role="panel"]').panel();
+			
+			
+			$( document ).on
+			(
+				'swipeleft swiperight',
+				'#demo-page',
+				function(event)
+				{
+					if ($('.ui-page-active').jqmData('panel')!=='open')
+					{
+						if (e.type==='swipeleft')
+						{
+							$('#right-panel').panel('open');
+						}
+						else if (e.type==='swiperight')
+						{
+							$('#left-panel').panel('open');
+						}
+					}
+				}
+			);
 		}
 	}
 );
